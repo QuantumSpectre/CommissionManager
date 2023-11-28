247d44d07eacf5b045a52e9765240e23e9679bd3
Charlie Snellings <charlie.snellings@gmail.com>
2023-11-28 11:26:35 -0500

    small sqlite changes

--- CommissionManagerApp/Models/UserAuthenticationRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CommissionManagerAPP.Models
-{
-    public class UserAuthenticationRequest
-    {
-        public string? Password { get; set; }
-        public string? Email { get; set; }
-    }
-}
