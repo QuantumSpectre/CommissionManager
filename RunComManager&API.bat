22ea88768ea62e0855d86ee5cb6993a77c745628
Charlie Snellings <charlie.snellings@gmail.com>
2023-11-30 12:36:56 -0500

    Rewrote batch file and removed .Exe files

---
+++ RunComManager&API.bat
@@ -0,0 +1,5 @@
+@echo off
+
+start dotnet run --project ".\CommissionManagerApp\CommissionManagerAPP.csproj"
+
+start dotnet run --project ".\CommissionManager.Gui\CommissionManager.GUI.csproj"
--- StartComManager.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0PublishedProjects\CommissionManager.GUI"
-start "" "CommissionManager.GUI.exe"
-
-cd /d "%~dp0PublishedProjects\CommissionManager.API"
-start "" "CommissionManagerAPP.exe"
